00174894cc085b7a4b35e87d58b9eaafebb7496f
gabrieudev <joao.gabriel@atoscapital.com.br>
2026-01-30 08:41:04 -0300

    chore(server): remover variáveis do Polar

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -7,10 +7,12 @@ export const env = createEnv({
     DATABASE_URL: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(32),
     BETTER_AUTH_URL: z.url(),
-    POLAR_ACCESS_TOKEN: z.string().min(1),
-    POLAR_SUCCESS_URL: z.url(),
+    //POLAR_ACCESS_TOKEN: z.string().min(1),
+    //POLAR_SUCCESS_URL: z.url(),
     CORS_ORIGIN: z.url(),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
