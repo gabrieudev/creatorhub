efabb7a75c0019bbbe8e26baf781108fa6dd850f
gabrieudev <joao.gabriel@atoscapital.com.br>
2026-02-02 11:24:10 -0300

    fix(server): alterar pacotes de db para suportar transaction

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,18 +1,17 @@
 import { env } from "@CreatorHub/env/server";
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
 
-import * as schema from "./schema/schema";
 import * as relations from "./schema/relations";
+import * as schema from "./schema/schema";
 
 neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
 
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql, {
+export const db = drizzle(env.DATABASE_URL, {
   schema: {
     ...schema,
     ...relations,
